dddcfc3cb4d7581bf077d4a7dd24b42998592bb6
guptan <nguptaa183@gmail.com>
2017-11-06 23:13:26 +0530

    fixes

--- howtoplay.php
+++ howtoplay.php
@@ -1,103 +1,103 @@
 <?php include "includes/header.php" ?>
 
 
-  <!-- navbar -->
-  <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item link">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link active" href="#" style="color:red;">How to Play</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="rules.html">Rules</a>
-          </li>
-        </ul>
-        <div class="btn-toolbar">
-          <button type="button" class="btn btn-danger">LOGIN</button>
-          <button type="button" class="btn btn-danger">SIGN UP</button>
-        </div>
-      </div>
-    </div>
-  </nav>
-
-
-
+<!-- navbar -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
   <div class="container">
-    <div class="jumbotron top">
-
-      <p align="justify">
-        Treasherlocked is an online cryptic hunt. The questions can be based on books, scientific notations, locations, different languages, important events and more. You don't need to be a coder or affiliated with any specific
-        subject to decode the answers. Search Engines are your biggest
-        rifles to win this cryptic war. A question will test your ability to decipher a very simple message hidden somewhere in your portal, rest you need to Power Search. Also, you will be provided with hints which will ultimately get you on the right path.
-
-        Once you have found the answer to a level you will be advanced to the next. As you progress through the levels, the questions might start getting trickier. The winner will be the person who uses provided hints and resources to the fullest and manages to cross all the level first.
-      </p>
-      <h3>Example</h3><br />
-      <p align="center">PUZZLE</p>
-      <p align="center"><img src="assets/img/picture.png" class="img-fluid" height="100px" width="100px" alt=""></p>
-
+    <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item link">
+          <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link active" href="#" style="color:red;">How to Play</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="rules.html">Rules</a>
+        </li>
+      </ul>
+      <div class="btn-toolbar">
+        <button type="button" class="btn btn-danger">LOGIN</button>
+        <button type="button" class="btn btn-danger">SIGN UP</button>
+      </div>
     </div>
-    <div class="jumbotron mid">
-      <h3 align="center">BREAKING THE PUZZLE</h4><br />
+  </div>
+</nav>
+
+
+
+<div class="container">
+  <div class="jumbotron top">
+
+    <p align="justify">
+      Treasherlocked is an online cryptic hunt. The questions can be based on books, scientific notations, locations, different languages, important events and more. You don't need to be a coder or affiliated with any specific
+      subject to decode the answers. Search Engines are your biggest
+      rifles to win this cryptic war. A question will test your ability to decipher a very simple message hidden somewhere in your portal, rest you need to Power Search. Also, you will be provided with hints which will ultimately get you on the right path.
+
+      Once you have found the answer to a level you will be advanced to the next. As you progress through the levels, the questions might start getting trickier. The winner will be the person who uses provided hints and resources to the fullest and manages to cross all the level first.
+    </p>
+    <h3>Example</h3><br />
+    <p align="center">PUZZLE</p>
+    <p align="center"><img src="assets/img/picture.png" class="img-fluid" height="100px" width="100px" alt=""></p>
+
+  </div>
+  <div class="jumbotron mid">
+    <h3 align="center">BREAKING THE PUZZLE</h4><br />
+      <ul>
+        <li>Let's decode the picture. In this picture, we have a word M. A quick Google search on keyword M tells us it could be:</li>
         <ul>
-          <li>Let's decode the picture. In this picture, we have a word M. A quick Google search on keyword M tells us it could be:</li>
-          <ul>
-            <li>James Bond - a fictional character</li>
-            <li>M (1931 film)</li>
-            <li>Roman representation of numeber 1000</li>
-            <p align="center">Let's try each of these as an answer.</p>
-          </ul>
-          <li>We got all wrong. Let's think more. James Bond, M (1931 film) or 1000 alone could lead to thousand of answers. There must be another hint. Let's try and find the name of the picture. You can see the page's source (hit Ctrl + U while in page) or use tools such as Inspect Element to do this. For this picture we have the name Picture.</li>
-          <li>Now let's relate the Picture with the earlier pieces of information. Search engine again best serves the purpose. At this point, googling picure 1000 will land you to the most sensible result which is a famous proverb "A picture is worth a thousand words." Let's try the proverb as an answer.</li>
-          <li>We got wrong again. But we are close. Let's think about the proverb again. Maybe like who authored it? Who did? Google says Arthur Brisbane did. Let's try that.</li>
-          <li>Bingo! That was the right answer.</li>
+          <li>James Bond - a fictional character</li>
+          <li>M (1931 film)</li>
+          <li>Roman representation of numeber 1000</li>
+          <p align="center">Let's try each of these as an answer.</p>
         </ul>
-      </div>
-      <p align="justify">Note: This tutorial uses Google as the search engine but you can freely choose your favorite one. Besides, since different search engines give you slightly different results, switching between different search engines might help you find the answer quicker.</p>
-      <div class="jumbotron last">
-        <h3>HOW TO ANSWER?</h3><br />
-        <p align="justify">
-          The answer, usually, is limited to few words. You submit answer in the format as you would in any regular occasion. Letter case and presence/absence of spaces or any other punctuation character won't affect the correctness of your answer. However, we cannot take care of the spelling mistakes you make.
-        </p>
-
-      </div>
-    </div>
-
-    <div class="footer">
-      <ul class="nav justify-content-center">
-        <li class="nav-item ">
-          <a class="nav-link active foot" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link foot" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link foot" href="#">Treasherlocked's Facebook</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link foot" href="#">Privacy Policy</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link foot" href="#">Contact Us</a>
-        </li>
-      </ul>
-      <ul class="nav justify-content-center foot">
-        <li>
-          2013 - 2017 &copy; <a href="http://www.microsoftcampusclub.in/" target="_blank">Microsoft Campus Club</a>
-          <em>(based in <a href="http://nitrkl.ac.in" target="_blank">National Institute of Technology Rourkela</a>)</em>
-        </li>
+        <li>We got all wrong. Let's think more. James Bond, M (1931 film) or 1000 alone could lead to thousand of answers. There must be another hint. Let's try and find the name of the picture. You can see the page's source (hit Ctrl + U while in page) or use tools such as Inspect Element to do this. For this picture we have the name Picture.</li>
+        <li>Now let's relate the Picture with the earlier pieces of information. Search engine again best serves the purpose. At this point, googling picure 1000 will land you to the most sensible result which is a famous proverb "A picture is worth a thousand words." Let's try the proverb as an answer.</li>
+        <li>We got wrong again. But we are close. Let's think about the proverb again. Maybe like who authored it? Who did? Google says Arthur Brisbane did. Let's try that.</li>
+        <li>Bingo! That was the right answer.</li>
       </ul>
     </div>
+    <p align="justify">Note: This tutorial uses Google as the search engine but you can freely choose your favorite one. Besides, since different search engines give you slightly different results, switching between different search engines might help you find the answer quicker.</p>
+    <div class="jumbotron last">
+      <h3>HOW TO ANSWER?</h3><br />
+      <p align="justify">
+        The answer, usually, is limited to few words. You submit answer in the format as you would in any regular occasion. Letter case and presence/absence of spaces or any other punctuation character won't affect the correctness of your answer. However, we cannot take care of the spelling mistakes you make.
+      </p>
 
-    <?php include "includes/footer.php" ?>
+    </div>
+  </div>
+
+  <div class="footer">
+    <ul class="nav justify-content-center">
+      <li class="nav-item ">
+        <a class="nav-link active foot" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link foot" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link foot" href="#">Treasherlocked's Facebook</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link foot" href="#">Privacy Policy</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link foot" href="#">Contact Us</a>
+      </li>
+    </ul>
+    <ul class="nav justify-content-center foot">
+      <li>
+        2013 - 2017 &copy; <a href="http://www.microsoftcampusclub.in/" target="_blank">Microsoft Campus Club</a>
+        <em>(based in <a href="http://nitrkl.ac.in" target="_blank">National Institute of Technology Rourkela</a>)</em>
+      </li>
+    </ul>
+  </div>
+
+  <?php include "includes/footer.php" ?>
