e8096376d6a0388660d3e5207f9b773a72b8da7b
guptan <nguptaa183@gmail.com>
2017-11-06 23:08:47 +0530

    fixes

--- index.php
+++ index.php
@@ -1,130 +1,130 @@
 <?php include "includes/header.php" ?>
 
-  <!-- navbar -->
-  <nav class="navbar fixed-top navbar-expand-md navbar-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item link">
-            <a class="nav-link" href="#about" style="color:red;">Home</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="howtoplay.html">How to Play</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="rules.html">Rules</a>
-          </li>
-        </ul>
-        <div class="btn-toolbar">
-          <button type="button" class="btn btn-danger">LOGIN</button>
-          <button type="button" class="btn btn-danger">SIGN UP</button>
-        </div>
-      </div>
-    </div>
-  </nav>
-
-  <!-- header img -->
-  <section class="first">
-    <div class="container">
-      <!-- Nikhil avatar -->
-      <div class="text-center">
-        <div class="avatar">
-          <img src="assets/img/ts/logo.png" class="img-fluid"/>
-          <p class="hunt">
-            because the hunt is on
-          </p>
-          <hr>
-        </div>
-        <button type="button" class="btn btn-danger">REGISTER NOW</button>
-      </div>
-    </div>
-  </section>
-
-
-  <div class="huntend">
-    <p>THE HUNT HAS ENDED</p>
-    <button type="button" class="btn btn-danger">SEE LEADERBOARD</button>
-  </div>
-
-
-  <div class="admin">
-    <div class="container">
-      <ul class="nav justify-content-center">
-        <li class="nav-item social">
-          <img src="assets/img/ts/acd.jpg" class="img-fluid rounded-circle" alt="img1"><br />
-          <p class="adminname">Admin ACD</p>
+<!-- navbar -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item link">
+          <a class="nav-link" href="#about" style="color:red;">Home</a>
         </li>
-        <li class="nav-item social">
-          <img src="assets/img/ts/sherlock.jpg" class="img-fluid rounded-circle" alt="img2"/><br />
-          <p class="adminname">Admin Sherlock</p>
-
+        <li class="nav-item link">
+          <a class="nav-link" href="about.html">About</a>
         </li>
-        <li class="nav-item social">
-          <img src="assets/img/ts/watson.jpg" class="img-fluid rounded-circle" alt="img3"><br />
-          <p class="adminname">Admin Watson</p>
-
+        <li class="nav-item link">
+          <a class="nav-link" href="howtoplay.html">How to Play</a>
         </li>
-        <li class="nav-item social">
-          <img src="assets/img/ts/moriarty.jpg" class="img-fluid rounded-circle" alt="img1"><br />
-          <p class="adminname">Admin Moriarty</p>
-
-        </li>
-        <li class="nav-item social">
-          <img src="assets/img/ts/mycroft.jpg" class="img-fluid rounded-circle" alt="img2"/><br />
-          <p class="adminname">Admin Mycroft</p>
-
+        <li class="nav-item link">
+          <a class="nav-link" href="rules.html">Rules</a>
         </li>
       </ul>
+      <div class="btn-toolbar">
+        <button type="button" class="btn btn-danger">LOGIN</button>
+        <button type="button" class="btn btn-danger">SIGN UP</button>
+      </div>
     </div>
   </div>
+</nav>
 
-
-  <div class="sponsor">
-    <div class="container">
-      <h2>Sponsors</h2>
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <img src="assets/img/ts/msclublogo.png" class="img-fluid" alt="msclublogo"/>
-
-        </li>
-      </ul>
+<!-- header img -->
+<section class="first">
+  <div class="container">
+    <!-- Nikhil avatar -->
+    <div class="text-center">
+      <div class="avatar">
+        <img src="assets/img/ts/logo.png" class="img-fluid"/>
+        <p class="hunt">
+          because the hunt is on
+        </p>
+        <hr>
+      </div>
+      <button type="button" class="btn btn-danger">REGISTER NOW</button>
     </div>
   </div>
+</section>
 
 
+<div class="huntend">
+  <p>THE HUNT HAS ENDED</p>
+  <button type="button" class="btn btn-danger">SEE LEADERBOARD</button>
+</div>
 
-  <div class="footer">
+
+<div class="admin">
+  <div class="container">
     <ul class="nav justify-content-center">
-      <li class="nav-item ">
-        <a class="nav-link active foot" href="#">Home</a>
+      <li class="nav-item social">
+        <img src="assets/img/ts/acd.jpg" class="img-fluid rounded-circle" alt="img1"><br />
+        <p class="adminname">Admin ACD</p>
       </li>
-      <li class="nav-item">
-        <a class="nav-link foot" href="#">About</a>
+      <li class="nav-item social">
+        <img src="assets/img/ts/sherlock.jpg" class="img-fluid rounded-circle" alt="img2"/><br />
+        <p class="adminname">Admin Sherlock</p>
+
       </li>
-      <li class="nav-item">
-        <a class="nav-link foot" href="#">Treasherlocked's Facebook</a>
+      <li class="nav-item social">
+        <img src="assets/img/ts/watson.jpg" class="img-fluid rounded-circle" alt="img3"><br />
+        <p class="adminname">Admin Watson</p>
+
       </li>
-      <li class="nav-item">
-        <a class="nav-link foot" href="#">Privacy Policy</a>
+      <li class="nav-item social">
+        <img src="assets/img/ts/moriarty.jpg" class="img-fluid rounded-circle" alt="img1"><br />
+        <p class="adminname">Admin Moriarty</p>
+
       </li>
-      <li class="nav-item">
-        <a class="nav-link foot" href="#">Contact Us</a>
-      </li>
-    </ul>
-    <ul class="nav justify-content-center foot">
-      <li>
-        2013 - 2017 &copy; <a href="http://www.microsoftcampusclub.in/" target="_blank">Microsoft Campus Club</a>
-        <em>(based in <a href="http://nitrkl.ac.in" target="_blank">National Institute of Technology Rourkela</a>)</em>
+      <li class="nav-item social">
+        <img src="assets/img/ts/mycroft.jpg" class="img-fluid rounded-circle" alt="img2"/><br />
+        <p class="adminname">Admin Mycroft</p>
+
       </li>
     </ul>
   </div>
+</div>
 
 
-  <?php include "includes/footer.php" ?>
+<div class="sponsor">
+  <div class="container">
+    <h2>Sponsors</h2>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <img src="assets/img/ts/msclublogo.png" class="img-fluid" alt="msclublogo"/>
+
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+
+<div class="footer">
+  <ul class="nav justify-content-center">
+    <li class="nav-item ">
+      <a class="nav-link active foot" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Treasherlocked's Facebook</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Privacy Policy</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Contact Us</a>
+    </li>
+  </ul>
+  <ul class="nav justify-content-center foot">
+    <li>
+      2013 - 2017 &copy; <a href="http://www.microsoftcampusclub.in/" target="_blank">Microsoft Campus Club</a>
+      <em>(based in <a href="http://nitrkl.ac.in" target="_blank">National Institute of Technology Rourkela</a>)</em>
+    </li>
+  </ul>
+</div>
+
+
+<?php include "includes/footer.php" ?>
