d71652e744007b33d7e3bbc92206584eee79f48e
guptan <nguptaa183@gmail.com>
2017-11-06 23:05:03 +0530

    fixes

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="assets/js/index.js"></script>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+
+  <!-- Title -->
+  <title>Treasherlocked - 5.0</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- favicon -->
+  <link rel="icon" type="image/gif" href="assets/img/ts/favicon.png" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- styling -->
+  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+  <!-- font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Raleway" rel="stylesheet">
+</head>
+<body>
