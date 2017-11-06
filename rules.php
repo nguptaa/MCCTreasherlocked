cfc010439e0aabb32b846d52a01b6efb2fc04cc4
guptan <nguptaa183@gmail.com>
2017-11-06 23:15:49 +0530

    fixes

--- rules.php
+++ rules.php
@@ -1,54 +1,54 @@
 <?php include "includes/header.php" ?>
 
 
-  <!-- navbar -->
-  <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item link">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link" href="howtoplay.html">How to Play</a>
-          </li>
-          <li class="nav-item link">
-            <a class="nav-link active" href="#" style="color:red;">Rules</a>
-          </li>
-        </ul>
-        <div class="btn-toolbar">
-          <button type="button" class="btn btn-danger">LOGIN</button>
-          <button type="button" class="btn btn-danger">SIGN UP</button>
-        </div>
+<!-- navbar -->
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="assets/img/ts/treasherlocked.png" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span><img src="assets/img/menu.svg" height="25" width="25" /></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item link">
+          <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="about.html">About</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link" href="howtoplay.html">How to Play</a>
+        </li>
+        <li class="nav-item link">
+          <a class="nav-link active" href="#" style="color:red;">Rules</a>
+        </li>
+      </ul>
+      <div class="btn-toolbar">
+        <button type="button" class="btn btn-danger">LOGIN</button>
+        <button type="button" class="btn btn-danger">SIGN UP</button>
       </div>
     </div>
-  </nav>
-
-
-
-  <div class="container">
-    <div class="jumbotron">
-
-
-      <h3 align="center">RULES</h3><br />
-      <ul class="rules">
-        <li>The event entails an Online Cryptic Treasure Hunt in which participants make their way through a series of cryptic levels.</li>
-        <li>The event shall be held over 3 days, starting from 28th October 2016. Check this page for regular updates.</li>
-        <li>Participation is open to everyone. Each participant represents himself/herself only.</li>
-        <li>The participants' aim is to crack the levels as quickly as they can so as to place themselves at the top of the leaderboard. They can make unlimited number of attempts at any level until they get the correct answer.</li>
-        <li>At each level, the participants will encounter a number of clues which shall all, together, point to one answer. Each level has one and only one correct answer. The answer will be limited to a word or a phrase sometimes.</li>
-        <li>The participants should beware of the spelling they enter, we cannot auto correct your spellings.</li>
-        <li>Sharing of answers with other participants in any form is banned. This also includes providing hints towards the clues/answer. The organizers reserve the right to disqualify or refuse participation to any participant without prior notice.</li>
-      </ul>
-
-    </div>
   </div>
+</nav>
 
-  <?php include "includes/footer.php" ?>
+
+
+<div class="container">
+  <div class="jumbotron">
+
+
+    <h3 align="center">RULES</h3><br />
+    <ul class="rules">
+      <li>The event entails an Online Cryptic Treasure Hunt in which participants make their way through a series of cryptic levels.</li>
+      <li>The event shall be held over 3 days, starting from 28th October 2016. Check this page for regular updates.</li>
+      <li>Participation is open to everyone. Each participant represents himself/herself only.</li>
+      <li>The participants' aim is to crack the levels as quickly as they can so as to place themselves at the top of the leaderboard. They can make unlimited number of attempts at any level until they get the correct answer.</li>
+      <li>At each level, the participants will encounter a number of clues which shall all, together, point to one answer. Each level has one and only one correct answer. The answer will be limited to a word or a phrase sometimes.</li>
+      <li>The participants should beware of the spelling they enter, we cannot auto correct your spellings.</li>
+      <li>Sharing of answers with other participants in any form is banned. This also includes providing hints towards the clues/answer. The organizers reserve the right to disqualify or refuse participation to any participant without prior notice.</li>
+    </ul>
+
+  </div>
+</div>
+
+<?php include "includes/footer.php" ?>
