ae93fe692e00b6b0fade5bb3f670f78982219493
guptan <nguptaa183@gmail.com>
2017-11-06 23:15:03 +0530

    fixes

--- rules.php
+++ rules.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
+<?php include "includes/header.php" ?>
 
-  <!-- Title -->
-  <title>Treasherlocked - 5.0</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- favicon -->
-  <link rel="icon" type="image/gif" href="assets/img/ts/favicon.png" />
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <!-- styling -->
-  <link rel="stylesheet" type="text/css" href="assets/css/rules.css">
-  <!-- font -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Raleway" rel="stylesheet">
-</head>
-<body>
 
   <!-- navbar -->
   <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
