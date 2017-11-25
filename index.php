6576c40f21bfd2949274d3a34887a5e8978c47ff
guptan <nguptaa183@gmail.com>
2017-11-26 00:49:43 +0530

    changes

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
   </div>
 </div>
 
-
+<!-- sponsers -->
 <div class="sponsor">
   <div class="container">
     <h2>Sponsors</h2>
@@ -98,4 +98,38 @@
   </div>
 </div>
 
-<?php include "includes/footer.php"; ?>
+<!-- footer -->
+<div class="footer">
+  <ul class="nav justify-content-center">
+    <li class="nav-item ">
+      <a class="nav-link active foot" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Treasherlocked's Facebook</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Privacy Policy</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link foot" href="#">Contact Us</a>
+    </li>
+  </ul>
+  <ul class="nav justify-content-center foot">
+    <li>
+      2013 - 2017 &copy; <a href="http://www.microsoftcampusclub.in/" target="_blank">Microsoft Campus Club</a>
+      <em>(based in <a href="http://nitrkl.ac.in" target="_blank">National Institute of Technology Rourkela</a>)</em>
+    </li>
+  </ul>
+</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/index.js"></script>
+
+</body>
+</html>
